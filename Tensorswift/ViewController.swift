bd778bb948a7cb280511fdc5017e4e1feea5fe14
mortenjust <morten.just@genstart.dk>
2017-01-09 22:36:21 -0800

    Initial Commit

---
+++ Tensorswift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tensorswift
+//
+//  Created by Morten Just Petersen on 1/9/17.
+//  Copyright © 2017 Morten Just Petersen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
