0b9581d4c5f0f7acbb7b981f38bfa57e304d71c8
mortenjust <morten.just@genstart.dk>
2017-03-11 12:02:59 -0800

    clean up viewcontroller

--- tensorswift/Tensorswift/ViewController.swift
+++ tensorswift/Tensorswift/ViewController.swift
@@ -14,32 +14,17 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     @IBOutlet weak var previewView: UIView!
     var bridge:TensorBridge = TensorBridge()
     
-//    AVCaptureVideoPreviewLayer *previewLayer;
-//    AVCaptureVideoDataOutput *videoDataOutput;
-//    dispatch_queue_t videoDataOutputQueue;
-//    AVCaptureStillImageOutput *stillImageOutput;
-//    UIView *flashView;
-//    UIImage *square;
-//    BOOL isUsingFrontFacingCamera;
-//    AVSpeechSynthesizer *synth;
 
-//    var session = AVCaptureSession()
-//    var previewLayer:AVCaptureVideoPreviewLayer!
-//    var videoDataOutput:AVCaptureVideoDataOutput!
-//    var videoDataOutputQueue:DispatchQueue!
-//    var stillImageOutput:AVCapturePhotoOutput! // AVCapturePhotoOutput from ios10
-//    var isUsingFrontFacingCamera:Bool!
-//  var AVCaptureStillImageIsCapturingStillImageContext = "AVCaptureStillImageIsCapturingStillImageContext"
-    
      private var videoCapture: VideoCapture!
         private var ciContext : CIContext!
-    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         guard let videoCapture = videoCapture else {return}
         videoCapture.startCapture()
     }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -53,17 +38,12 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
                                     previewContainer: previewView.layer)
      
         videoCapture.imageBufferHandler = {[unowned self] (imageBuffer, timestamp, outputBuffer) in
-        //    let ciImage = CIImage(cvPixelBuffer: imageBuffer)
-            
-           
-            // run cnn on pixelbuffer frame
-            
-            print("we havea  buffer")
             self.bridge.runCNN(onFrame: imageBuffer)
         }
-
-        
     }
+
+
+    // this function receives a set of seen objects and their probabilities
     
     func tensorLabelListUpdated(_ recognizedObjects:[AnyHashable : Any]){
         
@@ -74,90 +54,12 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             let confidence = seenObject.value as! Double
             print("Just saw \(label) with \(confidence)")
         }
-
-        
     }
     
-    
-    
-    
-    
-//    func setupAVCapture(){
-//        
-//        if UIDevice.current.userInterfaceIdiom == .phone {
-//            session.sessionPreset = AVCaptureSessionPreset640x480
-//        } else {
-//            session.sessionPreset = AVCaptureSessionPresetPhoto
-//        }
-//        
-//        let devices = AVCaptureDevice.devices()
-//        var device:AVCaptureDevice!
-//        
-//        // discover devices
-//        let deviceDiscoverySession = AVCaptureDeviceDiscoverySession(deviceTypes: [.builtInDualCamera, .builtInTelephotoCamera, .builtInWideAngleCamera], mediaType: AVMediaTypeVideo, position: .back)
-//        
-//        for device in (deviceDiscoverySession?.devices)! {
-//              if(device.position == AVCaptureDevicePosition.back){
-//                do {
-//                    let deviceInput = try AVCaptureDeviceInput(device: device)
-//                    isUsingFrontFacingCamera = false
-//                    if session.canAddInput(deviceInput) {
-//                        session.addInput(deviceInput)
-//                    }
-//                    stillImageOutput = AVCapturePhotoOutput()
-//                    
-//                    if session.canAddOutput(stillImageOutput) {
-//                        session.addOutput(stillImageOutput)
-//                    }
-//                    
-//                    let videoDataOutput = AVCaptureVideoDataOutput()
-//                    videoDataOutput.videoSettings = [kCVPixelBufferPixelFormatTypeKey as AnyHashable:kCVPixelFormatType_32BGRA]
-//                    videoDataOutput.alwaysDiscardsLateVideoFrames = true
-//                    
-//                    videoDataOutputQueue = DispatchQueue(label: "VideoDataOutputQueue")
-//                    videoDataOutput.setSampleBufferDelegate(self, queue: videoDataOutputQueue)
-//                    
-//                    if session.canAddOutput(videoDataOutput){
-//                        session.addOutput(videoDataOutput)
-//                    }
-//                    
-////                    videoDataOutput.connection(withMediaType: AVMediaTypeVideo).isEnabled = true
-//                    
-//                    previewLayer = AVCaptureVideoPreviewLayer(session: session)
-//                    previewLayer.backgroundColor = UIColor.black.cgColor
-//                    
-//                    previewLayer.videoGravity = AVLayerVideoGravityResizeAspect
-//                    
-//                    let rootLayer = CALayer(layer: previewLayer)
-//                    rootLayer.masksToBounds = true
-//                    previewLayer.frame = rootLayer.bounds
-//                    rootLayer.addSublayer(previewLayer)
-//                    
-//                    session.startRunning()
-//                } catch {
-//                    print("There was an errrrz setting up AVCaptureDeviceInput")
-//                    
-//                    return
-//                }
-//              } else {
-//                print("couldn't find a back camera")
-//            }
-//        }
-//    }
-//    
-//    func teardownAVCapture(){
-//        // release the queue?
-//        previewLayer.removeFromSuperlayer()
-//        
-//    }
-    
- 
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
